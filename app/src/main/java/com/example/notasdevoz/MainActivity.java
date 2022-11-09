ae3fff3f7394ee186bdd0df144fe3185b2b6de6b
carlos <carcanbla.alu@iesbenigaslo.es>
2022-11-09 12:58:13 +0100

    09/11/2022

--- app/src/main/java/com/example/notasdevoz/MainActivity.java
+++ app/src/main/java/com/example/notasdevoz/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.notasdevoz;
 
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Environment;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -17,8 +20,17 @@ import com.example.notasdevoz.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaRecorder recorder;
+    private File archivo;
+    private TextView txtEstado;
+    private MediaPlayer player;
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
@@ -35,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        txtEstado = (TextView) this.findViewById(R.id.txtEstado);
     }
 
     @Override
@@ -64,5 +78,43 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    public void grabar(View v) throws IOException {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        File rutaAudio = new File(Environment.getExternalStorageDirectory().getPath());
+        try {
+            archivo = File.createTempFile("temporal", ".3gp", rutaAudio);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        recorder.setOutputFile(archivo.getAbsolutePath());
+        recorder.prepare();
+        recorder.start();
+        txtEstado.setText("Grabando..");
+    }
+
+    public void detener(View v) {
+        recorder.stop();
+        recorder.release();
+        player = new MediaPlayer();
+        player.setOnCompletionListener((MediaPlayer.OnCompletionListener) this);
+        try {
+            player.setDataSource(archivo.getAbsolutePath());
+        } catch (IOException e) {
+        }
+        try {
+            player.prepare();
+        } catch (IOException e) {
+        }
+        txtEstado.setText("Listo para reproducir");
+    }
+
+    public void reproducir(View v) {
+        player.start();
+        txtEstado.setText("Reproduciendo");
     }
 }
